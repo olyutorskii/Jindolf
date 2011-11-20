bc79064ef84225a2596796e00b902f5e80e92d94
Olyutorskii <olyutorskii@users.osdn.me>
2011-11-20 14:20:56 +0900

    広告に関する文言を削除。

--- src/main/java/jp/sourceforge/jindolf/TopView.java
+++ src/main/java/jp/sourceforge/jindolf/TopView.java
@@ -106,8 +106,6 @@ public class TopView extends JPanel{
         warn.append("※ たまにはWebブラウザでアクセスして、");
         warn.append("<br></br>");
         warn.append("運営の動向を確かめようね！");
-        warn.append("<br></br>");
-        warn.append("広告収入にも貢献しよう！");
         warn.insert(0, "<center>").append("</center>");
         warn.insert(0, "<body>")  .append("</body>");
         warn.insert(0, "<html>")  .append("</html>");
