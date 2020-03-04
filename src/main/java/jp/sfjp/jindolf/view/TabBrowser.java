cf0a950cb7431517ee1a58452f376722de2d6159
Olyutorskii <olyutorskii@users.osdn.me>
2020-03-04 11:40:45 +0900

    apply map lambda.

--- src/main/java/jp/sfjp/jindolf/view/TabBrowser.java
+++ src/main/java/jp/sfjp/jindolf/view/TabBrowser.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
@@ -27,6 +28,12 @@ import jp.sfjp.jindolf.glyph.FontInfo;
 
 /**
  * タブを用いて村情報と各Periodを切り替え表示するためのコンポーネント。
+ *
+ * <p>村情報タブのビューはVillageInfoPanel、
+ * PeriodタブのビューはPeriodViewが担当する。
+ *
+ * <p>PeriodViewの描画下請Discussionへのアクセス、
+ * およびフォント管理、会話描画設定を提供する。
  */
 @SuppressWarnings("serial")
 public final class TabBrowser extends JTabbedPane{
@@ -51,40 +58,25 @@ public final class TabBrowser extends JTabbedPane{
         // Mac Aqua L&F ignore WRAP_TAB_LAYOUT
         setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
 
-        Border border = BorderFactory.createEmptyBorder(5, 5, 5, 5);
-        this.villageInfo.setBorder(border);
-
-        addTab("村情報", new JScrollPane(this.villageInfo));
+        JComponent infoPane = decorateVillageInfo();
+        addTab("村情報", infoPane);
 
         initTab();
 
         return;
     }
 
-    /**
-     * 村情報閲覧用のコンポーネントを更新する。
-     */
-    private void updateVillageInfo(){
-        Village target = getVillage();
-        this.villageInfo.updateVillage(target);
-        return;
-    }
 
     /**
-     * 村情報表示タブを選択表示する。
-     */
-    private void selectVillageInfoTab(){
-        setSelectedIndex(0);
-        return;
-    }
-
-    /**
-     * 設定された村を返す。
+     * 村情報表示コンポーネントを装飾する。
      *
-     * @return 設定された村
+     * @return 装飾済みコンポーネント
      */
-    public Village getVillage(){
-        return this.village;
+    private JComponent decorateVillageInfo(){
+        Border border = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+        this.villageInfo.setBorder(border);
+        JScrollPane result = new JScrollPane(this.villageInfo);
+        return result;
     }
 
     /**
@@ -97,77 +89,6 @@ public final class TabBrowser extends JTabbedPane{
 
         updateVillageInfo();
         selectVillageInfoTab();
-
-        repaint();
-        revalidate();
-
-        return;
-    }
-
-    /**
-     * PeriodViewインスタンスを生成する。
-     *
-     * <p>フォント設定、会話表示設定、各種リスナの設定が行われる。
-     *
-     * @param period Period
-     * @return PeriodViewインスタンス
-     */
-    private PeriodView buildPeriodView(Period period){
-        PeriodView result;
-
-        result = new PeriodView(period);
-        result.setFontInfo(this.fontInfo);
-        result.setDialogPref(this.dialogPref);
-
-        Discussion discussion = result.getDiscussion();
-        for(ActionListener listener : getActionListeners()){
-            discussion.addActionListener(listener);
-        }
-        for(AnchorHitListener listener : getAnchorHitListeners()){
-            discussion.addAnchorHitListener(listener);
-        }
-
-        return result;
-    }
-
-    /**
-     * 新規に村を設定する。
-     *
-     * <p>村のPeriod数に応じてタブの数は変化する。
-     *
-     * @param village 新しい村
-     */
-    public final void setVillage(Village village){
-        Village oldVillage = this.village;
-        if(oldVillage != null && village != oldVillage){
-            oldVillage.unloadPeriods();
-        }
-
-        this.village = village;
-        if(this.village == null){
-            initTab();
-            return;
-        }
-
-        if(this.village != oldVillage){
-            selectVillageInfoTab();
-        }
-
-        updateVillageInfo();
-
-        int periodNum = this.village.getPeriodSize();
-        modifyTabCount(periodNum);
-
-        for(int periodDays = 0; periodDays < periodNum; periodDays++){
-            Period period = this.village.getPeriod(periodDays);
-            PeriodView periodView = buildPeriodView(period);
-
-            int tabIndex = periodDaysToTabIndex(periodDays);
-            setComponentAt(tabIndex, periodView);
-
-            String caption = period.getCaption();
-            setTitleAt(tabIndex, caption);
-        }
 
         repaint();
         revalidate();
@@ -233,6 +154,105 @@ public final class TabBrowser extends JTabbedPane{
     }
 
     /**
+     * 設定された村を返す。
+     *
+     * @return 設定された村
+     */
+    public Village getVillage(){
+        return this.village;
+    }
+
+    /**
+     * 新規に村を設定する。
+     *
+     * <p>村のPeriod数に応じてタブの数は変化する。
+     *
+     * @param village 新しい村
+     */
+    public final void setVillage(Village village){
+        Village oldVillage = this.village;
+        if(oldVillage != null && village != oldVillage){
+            oldVillage.unloadPeriods();
+        }
+
+        this.village = village;
+        if(this.village == null){
+            initTab();
+            return;
+        }
+
+        if(this.village != oldVillage){
+            selectVillageInfoTab();
+        }
+
+        updateVillageInfo();
+
+        int periodNum = this.village.getPeriodSize();
+        modifyTabCount(periodNum);
+
+        for(int periodDays = 0; periodDays < periodNum; periodDays++){
+            Period period = this.village.getPeriod(periodDays);
+            PeriodView periodView = buildPeriodView(period);
+
+            int tabIndex = periodDaysToTabIndex(periodDays);
+            setComponentAt(tabIndex, periodView);
+
+            String caption = period.getCaption();
+            setTitleAt(tabIndex, caption);
+        }
+
+        repaint();
+        revalidate();
+
+        return;
+    }
+
+    /**
+     * 村情報閲覧用のコンポーネントを更新する。
+     */
+    private void updateVillageInfo(){
+        Village target = getVillage();
+        this.villageInfo.updateVillage(target);
+        return;
+    }
+
+    /**
+     * 村情報表示タブを選択表示する。
+     */
+    private void selectVillageInfoTab(){
+        setSelectedIndex(0);
+        return;
+    }
+
+    /**
+     * PeriodViewインスタンスを生成する。
+     *
+     * <p>フォント設定、会話表示設定、各種リスナの設定が行われる。
+     *
+     * @param period Period
+     * @return PeriodViewインスタンス
+     */
+    private PeriodView buildPeriodView(Period period){
+        Objects.nonNull(period);
+
+        PeriodView result;
+
+        result = new PeriodView(period);
+        result.setFontInfo(this.fontInfo);
+        result.setDialogPref(this.dialogPref);
+
+        Discussion discussion = result.getDiscussion();
+        for(ActionListener listener : getActionListeners()){
+            discussion.addActionListener(listener);
+        }
+        for(AnchorHitListener listener : getAnchorHitListeners()){
+            discussion.addAnchorHitListener(listener);
+        }
+
+        return result;
+    }
+
+    /**
      * PeriodView一覧を得る。
      *
      * @return PeriodView の List
@@ -247,32 +267,6 @@ public final class TabBrowser extends JTabbedPane{
             result.add(periodView);
         }
 
-        return result;
-    }
-
-    /**
-     * 現在タブ選択中のDiscussionを返す。
-     *
-     * <p>Periodに関係ないタブが選択されていたらnullを返す。
-     *
-     * @return 現在選択中のDiscussion
-     */
-    public Discussion currentDiscussion(){
-        int tabIndex = getSelectedIndex();
-        Discussion result = getDiscussion(tabIndex);
-        return result;
-    }
-
-    /**
-     * 現在タブ選択中のPeriodViewを返す。
-     *
-     * <p>Periodに関係ないタブが選択されていたらnullを返す。
-     *
-     * @return 現在選択中のPeriodView
-     */
-    public PeriodView currentPeriodView(){
-        int tabIndex = getSelectedIndex();
-        PeriodView result = getPeriodView(tabIndex);
         return result;
     }
 
@@ -296,6 +290,19 @@ public final class TabBrowser extends JTabbedPane{
     }
 
     /**
+     * 現在タブ選択中のPeriodViewを返す。
+     *
+     * <p>Periodに関係ないタブが選択されていたらnullを返す。
+     *
+     * @return 現在選択中のPeriodView
+     */
+    public PeriodView currentPeriodView(){
+        int tabIndex = getSelectedIndex();
+        PeriodView result = getPeriodView(tabIndex);
+        return result;
+    }
+
+    /**
      * 指定したタブインデックスに関連付けられたDiscussionを返す。
      *
      * <p>Periodに関係ないタブが指定されたらnullを返す。
@@ -312,7 +319,22 @@ public final class TabBrowser extends JTabbedPane{
     }
 
     /**
+     * 現在タブ選択中のDiscussionを返す。
+     *
+     * <p>Periodに関係ないタブが選択されていたらnullを返す。
+     *
+     * @return 現在選択中のDiscussion
+     */
+    public Discussion currentDiscussion(){
+        int tabIndex = getSelectedIndex();
+        Discussion result = getDiscussion(tabIndex);
+        return result;
+    }
+
+    /**
      * フォント描画設定を変更する。
+     *
+     * <p>設定は各PeriodViewに委譲される。
      *
      * @param fontInfo フォント
      */
@@ -320,7 +342,7 @@ public final class TabBrowser extends JTabbedPane{
         Objects.nonNull(fontInfo);
         this.fontInfo = fontInfo;
 
-        getPeriodViewList().forEach((periodView) -> {
+        getPeriodViewList().forEach(periodView -> {
             periodView.setFontInfo(this.fontInfo);
         });
 
@@ -330,13 +352,15 @@ public final class TabBrowser extends JTabbedPane{
     /**
      * 発言表示設定を変更する。
      *
+     * <p>設定は各PeriodViewに委譲される。
+     *
      * @param dialogPref 発言表示設定
      */
     public void setDialogPref(DialogPref dialogPref){
         Objects.nonNull(dialogPref);
         this.dialogPref = dialogPref;
 
-        getPeriodViewList().forEach((periodView) -> {
+        getPeriodViewList().forEach(periodView -> {
             periodView.setDialogPref(this.dialogPref);
         });
 
@@ -346,17 +370,18 @@ public final class TabBrowser extends JTabbedPane{
     /**
      * ActionListenerを追加する。
      *
+     * <p>配下のDiscussionへもリスナは登録される。
+     *
      * @param listener リスナー
      */
     public void addActionListener(ActionListener listener){
         this.listenerList.add(ActionListener.class, listener);
 
-        if(this.village == null) return;
-
-        getPeriodViewList().forEach((periodView) -> {
-            Discussion discussion = periodView.getDiscussion();
-            discussion.addActionListener(listener);
-        });
+        getPeriodViewList().stream()
+                .map(PeriodView::getDiscussion)
+                .forEach(discussion ->{
+                    discussion.addActionListener(listener);
+                });
 
         return;
     }
@@ -369,12 +394,11 @@ public final class TabBrowser extends JTabbedPane{
     public void removeActionListener(ActionListener listener){
         this.listenerList.remove(ActionListener.class, listener);
 
-        if(this.village == null) return;
-
-        getPeriodViewList().forEach((periodView) -> {
-            Discussion discussion = periodView.getDiscussion();
-            discussion.removeActionListener(listener);
-        });
+        getPeriodViewList().stream()
+                .map(PeriodView::getDiscussion)
+                .forEach(discussion ->{
+                    discussion.removeActionListener(listener);
+                });
 
         return;
     }
@@ -391,17 +415,18 @@ public final class TabBrowser extends JTabbedPane{
     /**
      * AnchorHitListenerを追加する。
      *
+     * <p>配下のDiscussionへもリスナは登録される。
+     *
      * @param listener リスナー
      */
     public void addAnchorHitListener(AnchorHitListener listener){
         this.listenerList.add(AnchorHitListener.class, listener);
 
-        if(this.village == null) return;
-
-        getPeriodViewList().forEach((periodView) -> {
-            Discussion discussion = periodView.getDiscussion();
-            discussion.addAnchorHitListener(listener);
-        });
+        getPeriodViewList().stream()
+                .map(PeriodView::getDiscussion)
+                .forEach(discussion -> {
+                    discussion.addAnchorHitListener(listener);
+                });
 
         return;
     }
@@ -414,12 +439,11 @@ public final class TabBrowser extends JTabbedPane{
     public void removeAnchorHitListener(AnchorHitListener listener){
         this.listenerList.remove(AnchorHitListener.class, listener);
 
-        if(this.village == null) return;
-
-        getPeriodViewList().forEach((periodView) -> {
-            Discussion discussion = periodView.getDiscussion();
-            discussion.removeAnchorHitListener(listener);
-        });
+        getPeriodViewList().stream()
+                .map(PeriodView::getDiscussion)
+                .forEach(discussion -> {
+                    discussion.removeAnchorHitListener(listener);
+                });
 
         return;
     }
